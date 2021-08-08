ecca3be82a2ee48897cb33dedb64a4a3cd0cedd1
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-08 11:44:15 +0200

    Add first components

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ItemListContainer = ( {greeting} ) => {
+
+    return (
+        <div>
+            <h2>{greeting}</h2>
+            <hr/>
+        </div>
+    );
+
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+export const NavBar = () => {
+    return (
+
+            <header className="navbar">
+                <h1>LOGO</h1>
+
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Cities</a>
+                    <a href="">About Us</a>
+                </nav>
+            </header>
+
+    );
+
+}
