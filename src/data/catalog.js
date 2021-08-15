cbd93377c65bcc654def14455b5fbfff4d993574
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-15 23:51:43 +0200

    Add images to catalog

--- src/data/catalog.js
+++ src/data/catalog.js
@@ -1,140 +1,161 @@
+import amsterdam from "../img/amsterdam.jpg"
+import athens from "../img/athens.jpg"
+import barcelona from "../img/barcelona.jpg"
+import berlin from "../img/berlin.jpg"
+import budapest from "../img/budapest.jpg"
+import copenhagen from "../img/copenhagen.jpg"
+import dublin from "../img/dublin.jpg"
+import helsinki from "../img/helsinki.jpg"
+import lisbon from "../img/lisbon.jpg"
+import london from "../img/london.jpg"
+import madrid from "../img/madrid.jpg"
+import milan from "../img/milan.jpg"
+import munich from "../img/munich.jpg"
+import paris from "../img/paris.jpg"
+import prague from "../img/prague.jpg"
+import rome from "../img/rome.jpg"
+import stockholm from "../img/stockholm.jpg"
+import vienna from "../img/vienna.jpg"
+import zagreb from "../img/zagreb.jpg"
+import zurich from "../img/zurich.jpg"
+
 export const catalog = [
     {
         id: 1,
-        img: "",
+        img: amsterdam,
         name: "Amsterdam",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 6
     },
     {
         id: 2,
-        img: "",
+        img: athens,
         name: "Athens",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 3,
-        img: "",
+        img: barcelona,
         name: "Barcelona",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 6
     },
     {
         id: 4,
-        img: "",
+        img: berlin,
         name: "Berlin",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 5,
-        img: "",
+        img: budapest,
         name: "Budapest",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 6,
-        img: "",
+        img: copenhagen,
         name: "Copenhagen",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 6
     },
     {
         id: 7,
-        img: "",
+        img: dublin,
         name: "Dublin",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 8,
-        img: "",
+        img: helsinki,
         name: "Helsinki",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 8
     },
     {
         id: 9,
-        img: "",
+        img: lisbon,
         name: "Lisbon",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 10,
-        img: "",
+        img: london,
         name: "London",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 7
     },
     {
         id: 11,
-        img: "",
+        img: madrid,
         name: "Madrid",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 6
     },
     {
         id: 12,
-        img: "",
+        img: milan,
         name: "Milan",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 7
     },
     {
         id: 13,
-        img: "",
+        img: munich,
         name: "Munich",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 7
     },
     {
         id: 14,
-        img: "",
+        img: paris,
         name: "Paris",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 7
     },
     {
         id: 15,
-        img: "",
+        img: prague,
         name: "Prague",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 16,
-        img: "",
+        img: rome,
         name: "Rome",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 6
     },
     {
         id: 17,
-        img: "",
+        img: stockholm,
         name: "Stockholm",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 8
     },
     {
         id: 18,
-        img: "",
+        img: vienna,
         name: "Vienna",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 8
     },
     {
         id: 19,
-        img: "",
+        img: zagreb,
         name: "Zagreb",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 5
     },
     {
         id: 20,
-        img: "",
+        img: zurich,
         name: "Zürich",
         desc: "Lorem ipsum dolor sit amet, consectetur adipisici elit",
         price: 8
