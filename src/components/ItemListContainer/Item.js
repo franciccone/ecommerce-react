c3b49abeeca137adfbd23356371f21455a6acd2f
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-16 19:28:29 +0200

    Add first styles to body and catalog

--- src/components/ItemListContainer/Item.js
+++ src/components/ItemListContainer/Item.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './Item.css';
 
 export const Item = ( {img, name, desc, price} ) => {
     return (
             <div className="card">
-                <img src={img} alt={name}/>
-                <h3>{name}</h3>
-                <p>{desc}</p>
-                <p>{price} €</p>
+                <img src={img} alt={name} className="card-img"/>
+                <div className="card-text">
+                    <h3 className="card-name">{name}</h3>
+                    <p className="card-desc">{desc}</p>
+                    <p className="card-price">{price} €</p>
+                </div>
             </div>
     )
 }
--- src/components/ItemListContainer/ItemList.js
+++ src/components/ItemListContainer/ItemList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Item } from './Item'
+import './ItemList.css';
 
 export const ItemList = ( {cities = []} ) => {
 
