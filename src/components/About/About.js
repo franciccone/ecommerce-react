c7b634de2cef5f19fad4e23d1d73e86eade3031c
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-24 15:23:16 +0200

    Change About Us title order

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,8 @@ export const About = ( ) => {
 
     return (
         <section className="about-container">
-            <h2 className="about-title">Changing the way of making tourism</h2>
             <img className="about-img" src={ImgAbout} alt="Tourist guides"/>
+            <h2 className="about-title">Changing the way of making tourism</h2>
             <article className="about-text">
                 <p className="about-p">Lorem ipsum dolor sit amet</p>
                 <p className="about-p">Lorem ipsum dolor sit amet</p>
