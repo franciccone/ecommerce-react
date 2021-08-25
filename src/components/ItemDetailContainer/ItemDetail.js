6eba7725b4ff78c3237d1e608bebbbd0fe1a82b9
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-25 20:57:33 +0200

    Add more styles to ItemDetail

--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ItemCount } from '../ItemCount/ItemCount';
 import './ItemDetail.css';
+import { RiMoneyDollarBoxLine } from 'react-icons/ri';
 
 export const ItemDetail = ({region, name, bigdesc, picture, price}) => {
 
     return (
-        <div className="city-container">
+        <section className="city-container">
             <img className="city-img" src={picture} alt={name}/>
             <div className="city-box">
                 <h2 className="city-name">{name}</h2>
-                <p className="city-price">Price: {price} €</p>
+                <p className="city-price"><RiMoneyDollarBoxLine className="icon-price"/> {price} €</p>
+                <ItemCount stock={100} initial={1} onAdd={() => console.log('BUY')}/>
+                <p className="city-desc">{bigdesc}</p>
+                <Link to={`/regions/${region}`} className="city-btn">Back</Link>
             </div>
-            <ItemCount stock={100} initial={1} onAdd={() => console.log('BUY')}/>
-            <p className="city-desc">{bigdesc}</p>
-
-            <Link to={`/regions/${region}`} className="city-btn">Back</Link>
-        </div>
+        </section>
     )
 }
