e44774da5dc3b806c93fcce773f760f03289fa6d
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-25 21:59:15 +0200

    Add more styles to ItemDetail

--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -11,8 +11,10 @@ export const ItemDetail = ({region, name, bigdesc, picture, price}) => {
             <img className="city-img" src={picture} alt={name}/>
             <div className="city-box">
                 <h2 className="city-name">{name}</h2>
+                <div className="city-ticket-box">
                 <p className="city-price"><RiMoneyDollarBoxLine className="icon-price"/> {price} €</p>
                 <ItemCount stock={100} initial={1} onAdd={() => console.log('BUY')}/>
+                </div>
                 <p className="city-desc">{bigdesc}</p>
                 <Link to={`/regions/${region}`} className="city-btn">Back</Link>
             </div>
