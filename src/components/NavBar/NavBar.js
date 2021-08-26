33c096fb0e1e521aec8206a2ecc39e3637af055c
Francisco Ciccone <franciscociccone4@gmail.com>
2021-08-26 20:07:16 +0200

    Made navbar responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import CartWidget from '../CartWidget/CartWidget';
-import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
     return (
 
-            <header className="navbar">
-                <NavLink to={"/"}><h1 className="logo">LOGO</h1></NavLink>
-
-                <nav className="menu">
-                    <NavLink to={"/"} className="menu-link">Home</NavLink>
-                    <NavLink to={"/region"} className="menu-link" exact activeClassName="active-class">Regions</NavLink>
-                    <NavLink to={"/region/east"} className="menu-link" exact activeClassName="active-class">East</NavLink>
-                    <NavLink to={"/region/scandinavia"} className="menu-link" exact activeClassName="active-class">Scandinavia</NavLink>
-                    <NavLink to={"/region/south"} className="menu-link" exact activeClassName="active-class">South</NavLink>
-                    <NavLink to={"/region/west"} className="menu-link" exact activeClassName="active-class">West</NavLink>
-                    <NavLink to={"/about"} className="menu-link" exact activeClassName="active-class">About Us</NavLink>
-                </nav>
-
-                <CartWidget />
-            </header>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to={"/"}>LOGO</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                        <NavDropdown title="Regions" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to={"/region/east"}>East</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to={"/region/scandinavia"}>Scandinavia</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to={"/region/south"}>South</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to={"/region/west"}>West</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link as={Link} to={"/about"}>About Us</Nav.Link>
+                    </Nav>
+                    <CartWidget/>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
     );
 
