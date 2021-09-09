935f906115760421f72dc453a2de2c338a292faf
Francisco Ciccone <franciscociccone4@gmail.com>
2021-09-09 17:09:29 +0200

    Update CartScreen display

--- src/components/CartScreen/CartScreen.js
+++ src/components/CartScreen/CartScreen.js
@@ -6,47 +6,49 @@ import './CartScreen.scss';
 
 export const CartScreen = () => {
 
-    const {cart, deleteFromCart, emptyCart, cartQuantity} = useContext(CartContext);
+    const {cart, deleteFromCart, emptyCart} = useContext(CartContext);
 
     return (
         <>
-        { cartQuantity != null ? (<section className="cart-screen-container">
-                                <div>
-                                    <h1 className="cart-screen-title">Your tours</h1>
-                                    <div className="cart-screen-container-full">
-                                        {cart.map(city => (
-                                            <div className="cart-screen-box" key={city.id}>
-                                                <img src={city.img} alt={city.name} className="card-screen-img"/>
-                                                <h3 className="cart-screen-name">{city.name}</h3>
-                                                <p className="cart-screen-text">Quantity: {city.quantity}</p>
-                                                <p className="cart-screen-text">Price: {city.price * city.quantity} €</p>
-                                                <BsFillTrashFill onClick={() => deleteFromCart(city.id)} className="cart-screen-trash"/>
-                                            </div>
-                                        ))}
-                                    </div>
+        { cart >= 0 ? (<section className="cart-screen-container">
 
-                                    <hr/>
+                        <div className="cart-screen-container-empty">
+                            <h1 className="cart-screen-title">Your cart is empty!</h1>
 
-                                    <button className="btn btn-danger btn-empty" onClick={emptyCart}>Empty cart</button>
+                            <hr/>
+
+                            <Link to="/regions">
+                            <button className="btn btn-danger btn-empty">Find your city</button>
+                            </Link>
+                        </div>
+
+                        </section>
+                        
+                        )   :   (
+                        
+                        <section className="cart-screen-container">
+                            <div>
+                                <h1 className="cart-screen-title">Your tours</h1>
+                                <div className="cart-screen-container-full">
+                                    {cart.map(city => (
+                                        <div className="cart-screen-box" key={city.id}>
+                                            <img src={city.img} alt={city.name} className="card-screen-img"/>
+                                            <h3 className="cart-screen-name">{city.name}</h3>
+                                            <p className="cart-screen-text">Quantity: {city.quantity}</p>
+                                            <p className="cart-screen-text">Price: {city.price * city.quantity} €</p>
+                                            <BsFillTrashFill onClick={() => deleteFromCart(city.id)} className="cart-screen-trash"/>
+                                        </div>
+                                    ))}
                                 </div>
-                            </section>)
 
-                            :   (<section className="cart-screen-container">
+                                <hr/>
 
-                                    <div className="cart-screen-container-empty">
-                                        <h1 className="cart-screen-title">Your cart is empty!</h1>
+                                <button className="btn btn-danger btn-empty" onClick={emptyCart}>Empty cart</button>
+                            </div>
+                        </section>)
 
-                                        <hr/>
-
-                                        <Link to="/regions">
-                                        <button className="btn btn-danger btn-empty">Find your city</button>
-                                        </Link>
-                                    </div>
-
-                                </section>)
         }
         </>
-            
         
     );
 };
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -24,7 +24,6 @@ export const ItemDetailContainer = () => {
             })
             .finally(()=> { setLoading(false)})
 
-
     }, [itemId, setLoading])
     
     return (
